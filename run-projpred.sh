33d61f53c2af67fe67cb0ebdba3e935ceed5405a
l-gorman <lgorman@turing.ac.uk>
2023-06-13 15:07:33 +0100

    adds slurm run scripts

---
+++ run-projpred.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+ 
+#SBATCH --job-name=overall_models
+#SBATCH -o ./Report/projpred.out
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=1-00:00:00
+#SBATCH --mem=8G
+#SBATCH --account=sscm012844
+
+
+cd "${SLURM_SUBMIT_DIR}"
+
+echo "Running on host $(hostname) \n"
+echo "Time is $(date) \n"
+echo "Directory is $(pwd) \n"
+echo "Slurm job ID is ${SLURM_JOBID} \n"
+echo "This jobs runs on the following machines: \n"
+echo "${SLURM_JOB_NODELIST}"
+
+echo "Keep track of job by entering sacct -j ${SLURM_JOBID}  \n"
+echo "Cancel your job by entering scancel ${SLURM_JOBID}  \n"
+echo "More details on submitting jobs here https://www.acrc.bris.ac.uk/protected/hpc-docs/job_types/ \n"
+
+module add languages/r/4.1.0
+
+Rscript "./run_projpred.R"
+
+

ac218ff57dfabfbf530c87f8af40b424946e47f3
l-gorman <lgorman@turing.ac.uk>
2023-06-29 09:24:04 +0100

    reduces run time in example

--- run-projpred.sh
+++ run-projpred.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
-#SBATCH --time=1-00:00:00
+#SBATCH --time=0-00:30:00
 #SBATCH --mem=16G
 #SBATCH --account=sscm012844
 #SBATCH --array=1-2
